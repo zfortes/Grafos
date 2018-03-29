ca1e5e822911d41ca6531d1bf22a03ebefbbfd6f
Jose <jose.fortes.neto@gmail.com>
2018-03-29 08:51:13 -0300

    Pequenas mudancas

--- Cod/main.c
+++ Cod/main.c
@@ -1,10 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "listas.h"
 #include "readgml.h"
 #include "network.h"
 #include <time.h>
 
+typedef struct eList{
+    int i;
+    struct eList *prox;
+}e_list;
+
+typedef struct list{
+    struct eList *primeiro;
+}t_list;
+
+t_list* criaLista();
+void inserirLista(t_list *lista, int n);
+
+t_list* criaLista(){
+    t_list *list = (t_list*) malloc(sizeof(list));
+    list->primeiro=NULL;
+    return list;
+}
+
+void inserirLista(t_list *lista, int n){
+    e_list *novoElemento = (e_list *) malloc(sizeof(e_list));
+
+    novoElemento->prox = lista->primeiro;
+    lista->primeiro = novoElemento;
+    novoElemento->i = n;
+
+
+    //if (lista->primeiro != NULL){
+    //    lista->primeiro = novoElemento;
+    //    novoElemento->i = n;
+    //    return ;
+    //}
+    //Irá percorrer toda a lista ate chegar na ultima posicao
+    //e_list *percorre = lista->primeiro;
+    //while (percorre->prox != NULL){
+    //    percorre = percorre->prox;
+    //}
+
+}
+//Le o grafo do arquivo e o salva em memoria
 int ler(){
 	char aux;
 	int i=0, j=0;
@@ -23,9 +61,21 @@ int ler(){
     read_network(network, rest);
 
 
-
+    /*
     printf("%d\n", network->nvertices);
+    printf("directed = %d\n", network->directed);
 
+    // imprime todos os ids dos vertices
+    printf("\n____ID_____\n", );
+    for (i=0; i < 34; i++){
+        printf("Id 2 = %d\n", network->vertex[i].id);
+    }
+    printf("\n___________\n" );
+
+    // imprime
+    printf("Id 2 = %d\n", network->vertex[0].id);
+    printf("Grau = %d\n", network->vertex[0].degree);
+    */
 
     fclose(rest);
 	return 0;
@@ -34,27 +84,22 @@ int ler(){
 int main(){
     int i = 0;
 
-    //t_list *lista = criaLista();
+    t_list *lista = criaLista();
 
 
 
     ler();
 
+   for (i; i<11; i++){
+       inserirLista(lista, i);
+   }
 
-
-
-
-
-
-//    for (i; i<11; i++){
-//        inserirLista(lista, i);
-//    }
-
-//    e_list *percorre = lista->primeiro;
-//    printf("passou\n" );
-//    while (percorre != NULL){
-//        printf("%d\n", percorre->i);
-//        percorre = percorre->prox;
-//    }
+   /*
+   e_list *percorre = lista->primeiro;
+   printf("passou\n" );
+   while (percorre != NULL){
+       printf("%d\n", percorre->i);
+       percorre = percorre->prox;
+   }*/
     return 0;
 }
